3da44ac14fdfe1c48be035543f11c012e14b734b
rodri <rodri@Rodrigo>
2024-05-20 19:06:23 -0300

    Commit1

---
+++ src/Model/Relatorio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Model.*;
+import DAO.*;
+import java.util.ArrayList;
+
+public class Relatorio {
+    
+    public static void ListaGeral(){
+        // this.GetListaTeclado();
+        // this.GetListaFone();
+        //carregaTabela()
+    }
+    //override de DAO Teclado;
+     public ArrayList getListaTeclado() {
+         repassa tabela!!!!!!!
+    
+
+    
+}
+
+}
